65f46dffad864b43081bab843a6738c851d5dcf3
ishi <berugen1st@gmail.com>
2023-10-30 00:44:09 +0700

    add ball and handle intersection between ball and racket

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+pub mod ball;
 pub mod constants;
 pub mod paddle;
+use ball::Ball;
 use constants::{CELL_SIZE, SCREEN_SIZE, TITLE};
 use paddle::Paddle;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use std::thread;
 use std::time::Duration;
 
@@ -22,6 +25,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut canvas = window.into_canvas().build()?;
     let mut player1 = Paddle::new(25, 0);
     let mut player2 = Paddle::new(width as i32 - (CELL_SIZE * 2), 0);
+    let mut ball = Ball::new(30, 30, 5, 5);
 
     canvas.clear();
     canvas.present();
@@ -29,6 +33,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
         canvas.clear();
+
+        ball.move_around();
 
         for event in event_pump.poll_iter() {
             match event {
@@ -60,6 +66,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         resolve_overflow(&mut player1);
         resolve_overflow(&mut player2);
 
+        bounce(&mut player1, &mut ball);
+        bounce(&mut player2, &mut ball);
+
+        canvas.set_draw_color(Color::RED);
+        canvas.fill_rect(ball.ball())?;
+
         canvas.set_draw_color(Color::WHITE);
         canvas.fill_rects(&player1.pad())?;
 
@@ -88,5 +100,23 @@ fn resolve_overflow(target: &mut Paddle) {
 
     if ly > (height as i32) - CELL_SIZE {
         target.set_y((y0 / 2) - 12);
+    }
+}
+
+fn bounce(paddle: &mut Paddle, ball: &mut Ball) {
+    for i in 0..paddle.pad().len() {
+        let part = paddle.pad()[i];
+        if detect_collision(&ball.ball(), &part) {
+            ball.set_vx(-ball.vx());
+            ball.x += ball.vx();
+        }
+    }
+}
+
+fn detect_collision(target: &Rect, opponent: &Rect) -> bool {
+    if target.has_intersection(*opponent) {
+        true
+    } else {
+        false
     }
 }
