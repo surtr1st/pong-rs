e19278105d90256ff05aa01e1b95b739194a23a5
ishi <berugen1st@gmail.com>
2023-10-29 19:05:39 +0700

    initialize game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+pub mod constants;
+pub mod paddle;
+use constants::{SCREEN_SIZE, TITLE};
+use paddle::Paddle;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::thread;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let (width, height) = SCREEN_SIZE;
+    let window = video_subsystem
+        .window(TITLE, width, height)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build()?;
+    let player1 = Paddle::new(0, 0);
+
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump()?;
+    'running: loop {
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode {
+                    Keycode::Q => break 'running,
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(Color::WHITE);
+        canvas.fill_rects(&player1.pad())?;
+
+        canvas.set_draw_color(Color::BLACK);
+        canvas.present();
+        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
 }
